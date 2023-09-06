89b85ef62bd98d7d284f80d0b7afbbf9ee0ab7b6
Lzzzd1 <luizbrabao123@gmail.com>
2023-09-05 22:56:11 -0300

    teste

--- src/__init__.py
+++ src/__init__.py
@@ -12,18 +12,12 @@ from src.adm import configure as adm_config
 from src.api import configure as api_config
 from src.models import Users, Cliente
 from src.cliente import configure as cliente_config
-from urllib.parse import quote
-
 
 
 def create_app():
     app = Flask(__name__)
-    FlaskDynaconf(app)
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://super:%s@base360-3365.postgres.pythonanywhere-services.com:13365/base360" % quote('@Oren2030405090batata')
-    app.config['SECRET_KEY'] = '78201b7c-148d-4d02-9cc4-564cdfa7bda3-ahshadasodjworkas;ld,masçlkfsdç;dfksçladl'
-    app.config['S3_ACCESS_KEY'] = "AKIA3QDQYWLITSHCCIOI"
-    app.config['S3_SECRET_KEY']= "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"
-    app.config['S3_BUCKET_NAME']= "base360crm"
+    FlaskDynaconf(app, env='testing')
+    # app.config.from_prefixed_env()
     db_config(app)
     app.config['DEBUG_TB_TEMPLATE_EDITOR_ENABLED'] = True
     app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
